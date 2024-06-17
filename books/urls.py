26da16e64518fff3328fadf3cba63bea3fe21d97
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-06-17 00:09:43 -0600

    Fixed api urls

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from .views import BookListCreate, BookDetail, home, book_detail
 urlpatterns = [
     path('', home, name='home'),
     path('books/<int:pk>/', book_detail, name='book_detail'),
-    path('books/api/', BookListCreate.as_view(), name='book_list_create'),
-    path('books/api/<int:pk>/', BookDetail.as_view(), name='book_detail'),
+    path('api/books/', BookListCreate.as_view(), name='book_list_create'),
+    path('api/books/<int:pk>/', BookDetail.as_view(), name='book_detail'),
 ]
