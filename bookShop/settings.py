7f390512208999b9687158f431f123fa884b66d0
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-06-16 23:38:51 -0600

    Fixed settings for the db

--- bookShop/settings.py
+++ bookShop/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'bookShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookShop',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '4444',
     }
 }
 
