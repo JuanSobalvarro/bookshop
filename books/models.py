c3cfdd3d39bcfd1cebc9fd9a8208ed8e96a110c8
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-06-16 22:45:18 -0600

    Init rep and added books app

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
