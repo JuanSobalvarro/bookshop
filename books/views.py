2f51234f3f425dcef0e9aba47f311913b4b9e600
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-06-16 23:26:28 -0600

    Adding serializers and views for books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+"""
+API VIEWS
+"""
+
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+"""
+WEBSITE VIEWS
+"""
+
+
+def home(request):
+    books = Book.objects.all()
+    return render(request, 'books/home.html', {'books': books})
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_detail.html', {'book': book})
